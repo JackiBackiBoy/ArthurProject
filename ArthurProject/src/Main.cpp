e5cf8ae5db466f0aa26b24a81da1cd674fe6c5c2
Fabian <cooltomten.fr@gmail.com>
2020-11-24 19:20:05 +0100

    Added X + arthurs

--- ArthurProject/src/Main.cpp
+++ ArthurProject/src/Main.cpp
@@ -5,4 +5,6 @@ int main()
 	std::cout << "Arthur" << std::endl;
 	std::cout << "Narthur" << std::endl;
 	std::cout << "Karthur" << std::endl;
+	std::cout << "Farthur" << std::endl;
+	std::cout << "Tarthur" << std::endl;
 }

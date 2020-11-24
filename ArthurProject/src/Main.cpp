1dd3152c316642bcd509d0daecd6d30c2fa0e4e6
JackiBackiBoy <TheGameTail@gmail.com>
2020-11-24 19:22:25 +0100

    Test

--- ArthurProject/src/Main.cpp
+++ ArthurProject/src/Main.cpp
@@ -6,4 +6,11 @@ int main()
 	std::cout << "Narthur" << std::endl;
 	std::cout << "Karthur" << std::endl;
 	std::cout << "Barthur" << std::endl;
+	std::cout << "Barthur" << std::endl;
+	std::cout << "Barthur" << std::endl;
+	std::cout << "Barthur" << std::endl;
+	std::cout << "Barthur" << std::endl;
+	std::cout << "Barthur" << std::endl;
+	std::cout << "Barthur" << std::endl;
+	std::cout << "Barthur" << std::endl;
 }

fc919da910692c6ff14f11d80d4f08ef6e9c48d2
Fabian <cooltomten.fr@gmail.com>
2020-11-24 22:51:01 +0100

    AssetManager not done, added temp assets

---
+++ ArthurProject/src/AssetManager.cpp
@@ -0,0 +1,11 @@
+#include "AssetManager.h"
+
+sf::Texture AssetManager::GetTexture(std::string aString)
+{
+	return sf::Texture();
+}
+
+sf::Font AssetManager::GetFont(std::string aString)
+{
+	return sf::Font();
+}

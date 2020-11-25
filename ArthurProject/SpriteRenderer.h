645aed55d7925a884dbd64d7cd454a779bec7e6b
Axel Andersson <Axel.O.Andersson@outlook.com>
2020-11-25 16:58:37 +0100

    SpriteRenderer framework done

---
+++ ArthurProject/SpriteRenderer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Node.h"
+class SpriteRenderer : public Node
+{
+public:
+	
+	SpriteRenderer(const sf::Vector2f& aPosition, Node* aParent, const sf::Texture& aTexture);
+	inline void OnUpdate() override { Node::OnUpdate(); }
+	void OnRender(sf::RenderWindow *aWindow) override;
+	void SetPosition(const sf::Vector2f& aPosition) override;
+protected:
+	sf::Sprite *aSprite;
+};
--- ArthurProject/src/Sandbox.cpp
+++ ArthurProject/src/Sandbox.cpp
@@ -18,7 +18,7 @@ public:
 
 	void OnRender() override
 	{
-
+ 
 	}
 };
 
